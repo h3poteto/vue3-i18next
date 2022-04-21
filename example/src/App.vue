2d3c845da679bff59e833b30092f96605a8f8582
AkiraFukushima <h3.poteto@gmail.com>
2022-04-21 21:31:39 +0900

    Add example vue application

---
+++ example/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <span class="hello">{{ helloText }}</span>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+  setup() {
+    return {
+      helloText: "Hello World!",
+    };
+  },
+});
+</script>
+
+<style>
+.hello {
+  color: red;
+}
+</style>
---
+++ example/webpack.config.js
@@ -0,0 +1,34 @@
+const { VueLoaderPlugin } = require("vue-loader");
+
+module.exports = {
+  mode: "development",
+  devtool: "inline-source-map",
+  entry: "./src/index.ts",
+  output: {
+    path: `${__dirname}/dist`,
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.ts$/,
+        loader: "ts-loader",
+        options: {
+          appendTsSuffixTo: [/\.vue$/],
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts"],
+  },
+  plugins: [new VueLoaderPlugin()],
+};
