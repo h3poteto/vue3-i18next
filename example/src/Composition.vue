3839d571008994f6780268abb7457a7b4a69e973
AkiraFukushima <h3.poteto@gmail.com>
2022-04-26 22:43:04 +0900

    refs #5 Add an example for composition API

---
+++ example/src/Composition.vue
@@ -0,0 +1,20 @@
+<template>
+  <p>{{ weather() }}</p>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { useI18next } from "vue3-i18next";
+
+export default defineComponent({
+  name: "Composition",
+  props: ["weather"],
+  setup(props) {
+    const i18n = useI18next();
+    const weather = () => i18n.t("top.weather", { weather: props.weather });
+    return {
+      weather,
+    };
+  },
+});
+</script>
